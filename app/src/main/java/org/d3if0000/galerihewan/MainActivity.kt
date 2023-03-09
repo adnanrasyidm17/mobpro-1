bd452022cecd467abccf96ec6c42bea3d5f29bc3
Adnan Debulz <adnanrasyidm@gmail.com>
2023-03-09 11:03:37 +0700

    Menambahkan View Binding

--- app/src/main/java/org/d3if0000/galerihewan/MainActivity.kt
+++ app/src/main/java/org/d3if0000/galerihewan/MainActivity.kt
@@ -3,16 +3,17 @@ package org.d3if0000.galerihewan
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
+import org.d3if0000.galerihewan.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         Log.d("MainActivity", "Jumlah Data: " + getData().size)
     }
