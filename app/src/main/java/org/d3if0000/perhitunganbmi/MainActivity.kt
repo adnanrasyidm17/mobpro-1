e4af3ff71d2d3f68858142336ee251dc36964136
Adnan Debulz <adnanrasyidm@gmail.com>
2023-03-29 23:58:53 +0700

    Memisahkan UI Code dan Logic Hitung BMI

--- app/src/main/java/org/d3if0000/perhitunganbmi/MainActivity.kt
+++ app/src/main/java/org/d3if0000/perhitunganbmi/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Toast
 import org.d3if0000.perhitunganbmi.databinding.ActivityMainBinding
+import org.d3if0000.perhitunganbmi.model.HasilBmi
 import org.d3if0000.perhitunganbmi.model.KategoriBmi
 
 class MainActivity : AppCompatActivity() {
@@ -30,18 +31,32 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, R.string.tinggi_invalid, Toast.LENGTH_LONG).show()
             return
         }
-        val tinggiCm = tinggi.toFloat() / 100
         val selectedId = binding.radioGroup.checkedRadioButtonId
         if (selectedId == -1) {
             Toast.makeText(this, R.string.gender_invalid, Toast.LENGTH_LONG).show()
             return
         }
-        val isMale = selectedId == R.id.priaRadioButton
-        val bmi = berat.toFloat() / (tinggiCm * tinggiCm)
-        val kategori = getKategori(bmi, isMale)
 
-        binding.bmiTextView.text = getString(R.string.bmi_x, bmi)
-        binding.kategoriTextView.text = getString(R.string.kategori_x, getKategoriLabel())
+        val result = hitungBmi(
+            berat.toFloat(),
+            tinggi.toFloat(),
+            selectedId == R.id.priaRadioButton
+        )
+
+        showResult(result)
+    }
+
+    private fun hitungBmi(berat: Float, tinggi: Float, isMale: Boolean): HasilBmi {
+        val tinggiCm = tinggi / 100
+        val bmi = berat / (tinggiCm * tinggiCm)
+        val kategori = getKategori(bmi, isMale)
+        return HasilBmi(bmi, kategori)
+    }
+
+    private fun showResult(result: HasilBmi) {
+        binding.bmiTextView.text = getString(R.string.bmi_x, result.bmi)
+        binding.kategoriTextView.text = getString(R.string.kategori_x,
+            getKategoriLabel(result.kategori))
     }
 
     private fun getKategori(bmi: Float, isMale: Boolean): KategoriBmi {
---
+++ app/src/main/java/org/d3if0000/perhitunganbmi/model/HasilBmi.kt
@@ -0,0 +1,6 @@
+package org.d3if0000.perhitunganbmi.model
+
+data class HasilBmi(
+    val bmi: Float,
+    val kategori: KategoriBmi
+)
