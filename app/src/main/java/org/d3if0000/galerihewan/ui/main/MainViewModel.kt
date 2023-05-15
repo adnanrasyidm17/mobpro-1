69d7c82979e87db44bc7ddda8ae8b8f909501bc2
Adnan Debulz <adnanrasyidm@gmail.com>
2023-05-15 23:09:08 +0700

    Mengambil Data dari Internet

--- app/src/main/java/org/d3if0000/galerihewan/ui/main/MainViewModel.kt
+++ app/src/main/java/org/d3if0000/galerihewan/ui/main/MainViewModel.kt
@@ -1,10 +1,15 @@
 package org.d3if0000.galerihewan.ui.main
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.d3if0000.galerihewan.R
 import org.d3if0000.galerihewan.model.Hewan
+import org.d3if0000.galerihewan.network.HewanApi
 
 class MainViewModel : ViewModel() {
 
@@ -12,6 +17,18 @@ class MainViewModel : ViewModel() {
 
     init {
         data.value = initData()
+        retrieveData()
+    }
+
+    private fun retrieveData() {
+        viewModelScope.launch (Dispatchers.IO) {
+            try {
+                val result = HewanApi.service.getHewan()
+                Log.d("MainViewModel", "Success: $result")
+            } catch (e: Exception) {
+                Log.d("MainViewModel", "Failure: ${e.message}")
+            }
+        }
     }
 
     private fun initData(): List<Hewan> {
