17d42411f658378d891606d6756c0f0c8d5d5912
Adnan Debulz <adnanrasyidm@gmail.com>
2023-05-29 23:29:02 +0700

    Mempelajari Daur Hidup Activity

--- app/src/main/java/org/d3if0000/galerihewan/MainActivity.kt
+++ app/src/main/java/org/d3if0000/galerihewan/MainActivity.kt
@@ -2,10 +2,38 @@ package org.d3if0000.galerihewan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.i("MainActivity", "onCreate dijalankan")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("MainActivity", "onStart dijalankan")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("MainActivity", "onResume dijalankan")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("MainActivity", "onPause dijalankan")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("MainActivity", "onStop dijalankan")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("MainActivity", "onDestroy dijalankan")
     }
 }
--- app/src/main/java/org/d3if0000/galerihewan/model/Hewan.kt
+++ app/src/main/java/org/d3if0000/galerihewan/model/Hewan.kt
@@ -1,7 +1,5 @@
 package org.d3if0000.galerihewan.model
 
-import org.d3if0000.galerihewan.R
-
 data class Hewan(
     val nama: String,
     val namaLatin: String,
