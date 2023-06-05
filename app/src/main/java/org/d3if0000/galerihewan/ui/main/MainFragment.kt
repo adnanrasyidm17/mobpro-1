8acdc30a489a0eb944fad86a453ce04e5f5941e6
Adnan Debulz <adnanrasyidm@gmail.com>
2023-06-05 23:26:59 +0700

    Meminta Izin Aplikasi

--- app/src/main/java/org/d3if0000/galerihewan/ui/main/MainFragment.kt
+++ app/src/main/java/org/d3if0000/galerihewan/ui/main/MainFragment.kt
@@ -1,13 +1,19 @@
 package org.d3if0000.galerihewan.ui.main
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
+import org.d3if0000.galerihewan.MainActivity
 import org.d3if0000.galerihewan.databinding.FragmentMainBinding
 import org.d3if0000.galerihewan.network.ApiStatus
 
@@ -56,11 +62,30 @@ class MainFragment : Fragment() {
             }
             ApiStatus.SUCCESS -> {
                 binding.progressBar.visibility = View.GONE
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    requestNotificationPermission()
+                }
             }
             ApiStatus.FAILED -> {
                 binding.progressBar.visibility = View.GONE
                 binding.networkError.visibility = View.VISIBLE
             }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun requestNotificationPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                MainActivity.PERMISSION_REQUEST_CODE
+            )
         }
     }
 }
